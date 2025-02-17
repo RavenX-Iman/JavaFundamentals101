1400a7d8b9f55771d6a64351382a28feaf086129
im_nt <imantahir707@gmail.com>
2025-02-17 23:56:42 +0500

    Add Datatype1 and Variables1 classes for basic data types and variable usage examples

---
+++ TheoryWork/Datatype1.java
@@ -0,0 +1,16 @@
+public class Datatype1 {
+    public static void main(String[] args) {
+        String name = "Tom";
+        int age = 10;
+        double height = 5.5;
+        char section = 'A';
+        boolean isStudent = true ;
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Height:"+height);
+        System.out.println("Section:"+section);
+        System.out.println(name+ " is a student. ("+isStudent + ")");
+    }
+}
+
+
---
+++ TheoryWork/Variables.java
@@ -0,0 +1,21 @@
+class Variables1{
+    public static void main(String[] args) {
+
+        /*Variable is a container that stores a value
+         *to declare a variable we must specify the datatype
+          and give it a name/identifier.
+         */
+
+        //declaring a variable:
+        int a; //int is data type and a is variable name
+
+        //initializing a variable:
+        a= 20; //the value stored in  variable a is 20
+
+        //or declaring and initializing a variable in one line:
+        int b = 30;
+
+        System.out.println(a); //printing value of a
+        System.out.println(b); //printing value of b
+    }
+}
