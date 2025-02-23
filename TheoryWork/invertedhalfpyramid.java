699c6ac6d8045b8f8f5417a70a8abfbb08604e93
im_nt <imantahir707@gmail.com>
2025-02-23 21:55:45 +0500

    Added inverted half pyramid program to display an inverted half pyramid shape using asterisks

---
+++ TheoryWork/invertedhalfpyramid.java
@@ -0,0 +1,13 @@
+public class invertedhalfpyramid {
+    public static void main(String[] args) {
+        for(int a=1;a<=5;a++){
+            for (int c = 5; c > a; c--) {
+                System.out.print(" ");
+            }
+            for (int b = 1; b <= a; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
