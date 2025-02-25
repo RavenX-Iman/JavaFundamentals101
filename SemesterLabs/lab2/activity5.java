11c043b70c6f0a6f3cecb4c6ee419a9e85e268ce
im_nt <imantahir707@gmail.com>
2025-02-25 18:17:46 +0500

    Refactored constants to use 'final' keyword for better code clarity

--- SemesterLabs/lab2/activity5.java
+++ SemesterLabs/lab2/activity5.java
@@ -2,11 +2,11 @@
 import java.util.Scanner;
 public class activity5 {
     public static void main(String[] args) {
-        final double pi= 3.14159;
+        final double pi= 3.14159; //final keyword is used to make a constant
         Scanner I =new Scanner(System.in);
         System.out.println("Enter a number for radius:");
         double radius=I.nextDouble();
-        double area =radius*radius*pi;
+        double area =radius*radius*pi; //area of circle formula
         System.out.println("The area of the circle of radius "+radius+" is: "+area);
         I.close();
     }
