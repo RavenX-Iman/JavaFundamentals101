d736cdf1662b016d82e574f37f92f94194c06b26
im_nt <imantahir707@gmail.com>
2025-02-28 23:38:50 +0500

    Added program to demonstrate typecasting in Java

---
+++ TheoryWork/typecasting.java
@@ -0,0 +1,12 @@
+// import java.util.Scanner;
+public class typecasting {
+    public static void main(String[] args) {
+        char ch='A';
+        // Scanner S1= new Scanner(System.in);
+        // ch=S1.next().charAt(0);
+
+// int b=(int)ch;
+        System.out.println((int)ch);
+
+    }
+}
